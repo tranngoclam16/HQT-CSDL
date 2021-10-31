a44845239dd19ceea24bab538854b049ceee3cd1
ngphvy01 <ngphvy01@users.noreply.github.com>
2021-10-31 12:08:27 +0700

    phân quyền

---
+++ privilege.sql
@@ -0,0 +1,27 @@
+﻿--ROLE
+--Đối tác:
+		--được quyền sửa thông tin cá nhân
+		--được thêm, xóa, sửa thông tin sản phẩm
+		--được sửa thông tin chi nhánh
+		--xem và cập nhật thông tin đơn hàng, chi tiết và tình trạng đơn hàng
+
+--Khách hàng: 
+			--được quyền sửa thông tin cá nhân
+			--Xem danh sách đối tác, danh sách chi nhánh, danh sách sản phẩm
+			--Xem tình trạng đơn hàng và chi tiết đơn hàng, thông tin đơn hàng của mình
+
+--Tài xế: 
+			--Sửa thông tin cá nhân
+			--Xem danh sách đơn hàng
+			--Cập nhật tình trạng đơn hàng
+			--Xem danh sách thu nhập của mình
+
+--Nhân viên:
+			--Xem danh sách đối tác
+			--Sửa 
+
+--Admin:
+			--Cập nhật thông tin tài khoản
+			--Thêm xóa sửa tài khoản admin, nhân viên
+			--Khóa và kích hoạt tài khoản
+			--Cấp quyền trên csdl và giao diện
