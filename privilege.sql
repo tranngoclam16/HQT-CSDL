0365b271c301401bfcb2aa5c6a3b03bdbd87151f
ngphvy01 <ngphvy01@users.noreply.github.com>
2021-11-04 16:33:41 +0700

    privilege

--- privilege.sql
+++ privilege.sql
@@ -1,9 +1,11 @@
-﻿--ROLE
+﻿use HT_DHCH_ONLINE
+GO
+--ROLE
 --Đối tác:
 		--được quyền sửa thông tin cá nhân
 		--được thêm, xóa, sửa thông tin sản phẩm
 		--được sửa thông tin chi nhánh
-		--xem và cập nhật thông tin đơn hàng, chi tiết và tình trạng đơn hàng
+		--xem  đơn hàng, chi tiết và cập nhật  tình trạng đơn hàng
 
 --Khách hàng: 
 			--được quyền sửa thông tin cá nhân
@@ -15,13 +17,36 @@
 			--Xem danh sách đơn hàng
 			--Cập nhật tình trạng đơn hàng
 			--Xem danh sách thu nhập của mình
+CREATE ROLE [TAIXE] AUTHORIZATION [dbo]
+GO
+GRANT SELECT,UPDATE ON TaiXe TO [TAIXE]
+GRANT SELECT,UPDATE ON TinhTrangDH TO [TAIXE]
+GRANT SELECT ON DonHang TO [TAIXE]
+GRANT SELECT ON ThuNhapTX TO [TAIXE]
+GO
 
 --Nhân viên:
 			--Xem danh sách đối tác
-			--Sửa 
+			--cập nhật hợp đồng
 
+CREATE ROLE [NHANVIEN] AUTHORIZATION [dbo]
+GO
+GRANT UPDATE,SELECT ON DoiTac TO [NHANVIEN];
+GO
+GRANT UPDATE,SELECT ON HopDong TO [NHANVIEN];
+GO
 --Admin:
 			--Cập nhật thông tin tài khoản
 			--Thêm xóa sửa tài khoản admin, nhân viên
 			--Khóa và kích hoạt tài khoản
 			--Cấp quyền trên csdl và giao diện
+
+CREATE ROLE [roleDBA] AUTHORIZATION dbo;
+
+ALTER ROLE [db_owner] ADD MEMBER [roleDBA]
+ALTER ROLE [db_accessadmin] ADD MEMBER [roleDBA]
+ALTER ROLE [db_securityadmin] ADD MEMBER [roleDBA]
+GO
+
+
+
