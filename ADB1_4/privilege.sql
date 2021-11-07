cca743844117f8126fecb7883e71a1327bf199eb
tranngoclam16 <63225101+tranngoclam16@users.noreply.github.com>
2021-11-07 20:46:22 +0700

    nd tuần 2, 3, 4

---
+++ ADB1_4/privilege.sql
@@ -0,0 +1,79 @@
+﻿use HT_DHCH_ONLINE
+GO
+--ROLE
+--Đối tác:
+create role [DOITAC] authorization [dbo]
+go
+		--cấp quyền
+	--được quyền sửa thông tin cá nhân
+	grant select, update on DoiTac to [DOITAC]
+	--được xem,thêm, xóa, sửa thông tin sản phẩm
+	grant select, insert, delete, update on SanPham to [DOITAC]
+	--được xem, thêm, xóa,sửa thông tin chi nhánh
+	grant select,insert, delete, update on ChiNhanh to [DOITAC]
+	grant select,insert,delete,update on CN_SP to[DOITAC]
+	--xem thông tin đơn hàng, chi tiết và cập nhật tình trạng đơn hàng
+	grant select  on DonHang to [DOITAC]
+	grant select on CT_DonHang to [DOITAC]
+	grant select on CT_TTDH to [DOITAC]
+	grant select, update on TinhTrangDH to [DOITAC]
+
+
+
+--Khách hàng: 
+create role [KHACHHANG] authorization [dbo]
+go
+		--cấp quyền
+	--được quyền sửa thông tin cá nhân
+	grant select, update on KhachHang to [KHACHHANG] 
+	--Xem danh sách đối tác, danh sách chi nhánh, danh sách sản phẩm
+	grant select on DoiTac to [KHACHHANG]
+	grant select on ChiNhanh to [KHACHHANG]
+	grant select on SanPham to [KHACHHANG]
+	grant select on CN_SP to[KHACHHANG]
+	--Xem tình trạng đơn hàng và chi tiết đơn hàng, thông tin đơn hàng của mình
+	--Được thêm đơn hàng
+	grant select,insert on DonHang to [KHACHHANG]
+	grant select,insert on CT_DonHang to [KHACHHANG]
+	grant select on TinhTrangDH to [KHACHHANG]
+	grant select on CT_TTDH to [KHACHHANG]
+
+--Tài xế: 
+			--Sửa thông tin cá nhân
+			--Xem danh sách đơn hàng
+			--Cập nhật tình trạng đơn hàng
+			--Xem danh sách thu nhập của mình
+CREATE ROLE [TAIXE] AUTHORIZATION [dbo]
+GO
+GRANT SELECT,UPDATE ON TaiXe TO [TAIXE]
+GRANT SELECT,UPDATE ON TinhTrangDH TO [TAIXE]
+GRANT SELECT ON DonHang TO [TAIXE]
+GRANT SELECT, INSERT,DELETE ON ThuNhapTX TO [TAIXE]
+grant select on CT_TTDH to [TAIXE]
+GO
+
+--Nhân viên:
+			--Xem danh sách đối tác
+			--cập nhật hợp đồng
+
+CREATE ROLE [NHANVIEN] AUTHORIZATION [dbo]
+GO
+GRANT SELECT ON DoiTac TO [NHANVIEN];
+GO
+GRANT UPDATE,SELECT ON HopDong TO [NHANVIEN];
+GO
+--Admin:
+			--Cập nhật thông tin tài khoản
+			--Thêm xóa sửa tài khoản admin, nhân viên
+			--Khóa và kích hoạt tài khoản
+			--Cấp quyền trên csdl và giao diện
+
+CREATE ROLE [roleDBA] AUTHORIZATION dbo;
+
+ALTER ROLE [db_owner] ADD MEMBER [roleDBA]
+ALTER ROLE [db_accessadmin] ADD MEMBER [roleDBA]
+ALTER ROLE [db_securityadmin] ADD MEMBER [roleDBA]
+GO
+
+
+
---
+++ ADB1_4/trigger.sql
@@ -0,0 +1,144 @@
+﻿USE HT_DHCH_ONLINE
+GO
+--Thành tiền
+create trigger tinh_thanh_tien 
+on CT_DonHang
+for insert, update
+as
+begin
+	update CT_DonHang
+	set GiaBan = (select sp.GiaBan
+					from SanPham sp 
+					where CT_DonHang.MaSP = sp.MaSP)
+	from INSERTED i
+	where i.MaDH= CT_DonHang.MaDH and i.MaSP= CT_DonHang.MaSP
+
+	update CT_DonHang
+	set ThanhTien=i.SoLuong* CT_DonHang.GiaBan
+	from INSERTED i 
+	where i.MaDH=CT_DonHang.MaDH and i.MaSP=CT_DonHang.MaSP;
+end
+go
+
+--Tổng tiền =phí vận chuyển + sum (thành tiền)
+CREATE TRIGGER insertCTHD on CT_DonHang
+for INSERT,UPDATE
+AS
+BEGIN
+	update DonHang
+	set TongHang= (select sum (ct.ThanhTien)
+							from CT_DonHang ct
+							where ct.MaDH= DonHang.MaDH)
+	from INSERTED
+	where INSERTED.MaDH= DonHang.MaDH
+
+	UPDATE DonHang
+	SET PhiVanChuyen = (select dh.TongHang * 0.05
+							from DonHang dh
+							where dh.MaDH= DonHang.MaDH)
+	FROM INSERTED
+	where INSERTED.MaDH= DonHang.MaDH
+
+END
+GO
+
+CREATE TRIGGER deleteCTHD on CT_DonHang
+FOR DELETE
+AS 
+BEGIN
+	update DonHang
+	set TongHang= (select sum (ct.ThanhTien)
+							from CT_DonHang ct
+							where ct.MaDH= DonHang.MaDH)
+	from DELETED 
+	where  DELETED.MaDH= DonHang.MaDH
+
+	UPDATE DonHang
+	SET PhiVanChuyen = (select dh.TongHang * 0.05
+							from DonHang dh
+							where dh.MaDH= DonHang.MaDH)
+	FROM  DELETED
+	where  DELETED.MaDH= DonHang.MaDH
+END
+GO
+
+--số chi nhánh:
+--số chi nhánh lúc đầu =0
+create trigger insertDT on DoiTac 
+for insert 
+as
+begin
+	update DoiTac set SoChiNhanh=0
+	from INSERTED 
+	where INSERTED.MaDT=DoiTac.MaDT
+end
+GO
+--số chi nhánh được cập nhật theo bảng chi nhánh
+CREATE TRIGGER insertCN on ChiNhanh
+FOR INSERT
+AS
+BEGIN
+	update DoiTac
+	set DoiTac.SoChiNhanh= (select count(ChiNhanh.MaCN) from ChiNhanh 
+							where MaDT= DoiTac.MaDT)
+	from INSERTED i 
+	where i.MaDT= DoiTac.MaDT
+END
+GO
+
+CREATE TRIGGER deleteCN on ChiNhanh
+FOR DELETE
+AS
+BEGIN
+	update DoiTac
+	set DoiTac.SoChiNhanh= (select count(ChiNhanh.MaCN) from ChiNhanh 
+							where MaDT= DoiTac.MaDT)
+	from DELETED d
+	where d.MaDT= DoiTac.MaDT
+END
+GO
+
+
+
+
+-------------------------------------------------------------------------------------------------------------------------------------------
+---trigger cập nhật số lượng tồn
+-- khi đặt hàng
+CREATE TRIGGER slt_dathang ON CT_DonHang for INSERT AS 
+BEGIN
+		UPDATE SanPham
+		SET SLTon = SLTon - (
+		SELECT sum(SoLuong)
+		FROM INSERTED i
+		WHERE i.MaSP = SanPham.MaSP
+	)
+		FROM INSERTED i
+		where SanPham.MaSP = i.MaSP
+		select * from SanPham
+END
+GO
+
+-- khi hủy hàng
+CREATE TRIGGER slt_huyhang ON CT_DonHang FOR DELETE AS 
+BEGIN
+	UPDATE SanPham
+	SET SLTon = SLTon + (
+		SELECT sum(SoLuong)
+		FROM deleted d
+		WHERE d.MaSP = SanPham.MaSP
+	)
+	FROM DELETED d 
+	where SanPham.MaSP = d.MaSP
+END
+GO
+-- khi sửa chi tiết hóa đơn
+CREATE TRIGGER slt_capnhathang ON CT_DonHang after update AS
+BEGIN
+   UPDATE SanPham 
+   SET SLTon = SLTon -
+	   (SELECT sum(SoLuong) FROM inserted WHERE MaSP = SanPham.MaSP) +
+	   (SELECT sum(SoLuong) FROM deleted WHERE MaSP = SanPham.MaSP)
+   FROM SanPham
+   JOIN deleted ON SanPham.MaSP = deleted.MaSP
+END
+GO
