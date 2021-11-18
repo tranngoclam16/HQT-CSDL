07c3d50c13932038e00ec93c5639aebc42100e29
tranngoclam16 <63225101+tranngoclam16@users.noreply.github.com>
2021-11-18 18:30:26 +0700

    update tranh chấp

---
+++ Lam_tranhchap/Tran1_TestCase03.sql
@@ -0,0 +1,36 @@
+﻿create procedure sp_XemTTDonHang
+	(@MaDH varchar(10), @TTDH nvarchar(100) output)
+as
+begin
+	begin tran
+		begin try
+			declare @MaTT int
+			select @MaTT = MaTT
+			from TinhTrangDH
+			where MaDH = @MaDH and CAST(NgayCapNhat as datetime) >= All(select CAST (NgayCapNhat as datetime)
+																from TinhTrangDH
+																where MaDH = @MaDH)
+			select @TTDH =  Mota from CT_TTDH where MaTinhTrang = @MaTT
+		end try
+		begin catch
+			IF @@trancount>0
+				BEGIN	
+					print(N'Lỗi')
+					ROLLBACK TRANSACTION 
+				END
+		end catch
+	commit tran
+	return
+end
+
+go
+declare @TTDonHang nvarchar(100);	
+exec sp_XemTTDonHang 'DH0000', @TTDonHang output;
+print (@TTDonHang)
+
+drop procedure sp_XemTTDonHang
+--Insert into TinhTrangDH values
+--(GETDATE(), 'DH0000', 1)
+--waitfor delay '00:00:02'
+--Insert into TinhTrangDH values
+--(GETDATE(), 'DH0000', 2)
---
+++ Lam_tranhchap/Tran2_TestCase03.sql
@@ -0,0 +1,31 @@
+﻿create procedure sp_CapNhatTTDonHang
+	(@MaDH varchar(10), @MaTT int)
+as
+begin
+	begin tran
+		begin try
+			insert into TinhTrangDH
+			values (GETDATE(), @MaDH, @MaTT)
+			waitfor delay '00:00:07'
+			ROLLBACK TRANSACTION 
+		end try
+		begin catch
+			IF @@trancount>0
+				BEGIN	
+					print(N'Lỗi')
+					ROLLBACK TRANSACTION 
+				END
+		end catch
+end
+
+go
+	
+exec sp_CapNhatTTDonHang 'DH0000', 3;
+
+
+drop procedure sp_CapNhatTTDonHang
+--Insert into TinhTrangDH values
+--(GETDATE(), 'DH0000', 1)
+--waitfor delay '00:00:02'
+--Insert into TinhTrangDH values
+--(GETDATE(), 'DH0000', 2)
